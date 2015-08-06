4f0f620dedefb3c7fe313e02a9e7c1cbdf1f58a6
bighappyface <anomalous20@gmail.com>
2015-08-06 10:50:06 -0500

    chore(release): 0.2.0

--- dist/ng-intl-tel-input.js
+++ dist/ng-intl-tel-input.js
@@ -1,12 +1,21 @@
 angular.module('ngIntlTelInput', []);
 angular.module('ngIntlTelInput')
-  .directive('ngIntlTelInput', function () {
+  .directive('ngIntlTelInput', ['$log', function ($log) {
     return {
       restrict: 'A',
-      require: "ngModel",
+      require: 'ngModel',
       link: function (scope, elm, attr, ctrl) {
         if (attr.type !== 'text') return;
+        // Initialize.
         elm.intlTelInput();
+        // Validation.
+        ctrl.$validators.ngIntlTelInput = function (value) {
+          return elm.intlTelInput("isValidNumber");
+        };
+        // Set model value to valid, formatted version.
+        ctrl.$parsers.push(function (value) {
+          return elm.intlTelInput('getNumber');
+        });
       }
     };
-  });
+  }]);
